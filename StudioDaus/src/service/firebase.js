a1c2dee9c5aeb9044056fb87597575759443e40d
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2024-10-31 19:34:07 -0300

    implantação do firebase

--- StudioDaus/src/service/firebase.js
+++ StudioDaus/src/service/firebase.js
@@ -1,18 +1,34 @@
-// Import the functions you need from the SDKs you need
+// Importa e inicializa o Firebase
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, push } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDT926A7CmQ4LNlNEGntJmB1vJ2tqvPidc",
-  authDomain: "daus-bbaa5.firebaseapp.com",
-  databaseURL: "https://daus-bbaa5-default-rtdb.firebaseio.com",
-  projectId: "daus-bbaa5",
-  storageBucket: "daus-bbaa5.firebasestorage.app",
-  messagingSenderId: "631298869552",
-  appId: "1:631298869552:web:bd028ba32541ccb2f160f5"
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+function salvarDados(email, link, respostaIA) {
+    const usuariosRef = ref(database, 'usuarios');
+
+    const novoUsuario = {
+        email: email,
+        link: link,
+        respostaIA: respostaIA || "" 
+    };
+
+    push(usuariosRef, novoUsuario)
+        .then(() => {
+            console.log("Novo usuário adicionado com sucesso!");
+        })
+        .catch((error) => {
+            console.error("Erro ao adicionar novo usuário:", error);
+        });
+}
